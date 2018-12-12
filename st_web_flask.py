07863d029e0604c577366737e4cdfd2d9c3c6aeb
bindu21 <bindu21india@gmail.com>
2018-12-11 21:29:01 -0500

    uodate

--- st_web_flask.py
+++ st_web_flask.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route('/')
-def my_fun():
-	return "I am Home page"
+def main_fun():
+	return render_template("home.html")
 	
 @app.route('/about/')
-def new_fun():
-	return "I am now about page"
+def about_fun():
+	return render_template("about.html")
 	
 if __name__ == "__main__":
 	app.run(debug=True)
